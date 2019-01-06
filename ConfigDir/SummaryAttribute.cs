b1cb7677976e588b8c3e5453cd75b434371e4485
Anton <ak8ton@gmail.com>
2019-01-06 20:35:42 +0500

    SummaryAttribute

---
+++ ConfigDir/SummaryAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConfigDir
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class SummaryAttribute : Attribute
+    {
+        public string Summary { get; }
+
+        public SummaryAttribute(string summary)
+        {
+            Summary = summary;
+        }
+    }
+}
--- UnitTests/Model/Configuration.cs
+++ UnitTests/Model/Configuration.cs
@@ -4,8 +4,8 @@ namespace UnitTests.Model
 {
     public abstract class Configuration : Config
     {
+        [Summary("Настройки тестового стенда")]
         public abstract IStend Stend { get; }
         public abstract IFilename Filename { get; }
-
     }
 }
