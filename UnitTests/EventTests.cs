9d0a774d1b562f42db83d7f8c24272ff23686252
Anton <ak8ton@gmail.com>
2019-01-09 20:27:27 +0500

    UnitTests EventTests

---
+++ UnitTests/EventTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConfigDir;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EventTests : TestBase
+    {
+        [TestMethod]
+        [ExpectedException(typeof(StopTestException))]
+        public void ValueFound()
+        {
+            var cfg = Config.GetOrCreate<Model.Configuration>("Config");
+
+            cfg.Data.OnValueFound += (o) => 
+            {
+                Assert.AreEqual("Config/Stend/Name", o.Path);
+                Assert.AreEqual("Stend1", o.Value);
+                Assert.AreEqual("Stend1", o.RawValue);
+                Assert.AreEqual(typeof(string), o.ExpectedType);
+                Assert.AreEqual(null, o.RequiredPath);
+                Assert.AreEqual("XML файл: Config\\Stend.xml", o.Source.Description);
+
+                throw new StopTestException();
+            };
+
+            var s = cfg.Stend.Name;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(StopTestException))]
+        public void ValueNotFound()
+        {
+            var cfg = Config.GetOrCreate<Model.Configuration>("Config");
+
+            cfg.Data.OnValueNotFound += (o) =>
+            {
+                throw new StopTestException();
+            };
+
+            var s = cfg.Filename.Options.Value5;
+        }
+    }
+}
---
+++ UnitTests/StopTestException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UnitTests
+{
+    public class StopTestException : Exception
+    {
+        public StopTestException() : base() { }
+    }
+}
