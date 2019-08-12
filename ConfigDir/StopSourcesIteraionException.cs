dfcc0fdbef244864c18c4a1e27f0482af1a687f7
Anton <ak8ton@gmail.com>
2019-08-12 21:53:32 +0500

    add StopSourcesIteraionException

---
+++ ConfigDir/StopSourcesIteraionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConfigDir
+{
+    /// <summary>
+    /// Искличение используемое в ISource для реализации атрибута "override"
+    /// </summary>
+    public class StopSourcesIteraionException : Exception
+    {
+        /// <summary>
+        /// Конструктор исключения StopSourcesIteraionException 
+        /// </summary>
+        public StopSourcesIteraionException()
+        {
+        }
+    }
+}
